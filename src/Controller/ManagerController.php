3dc6e92f7fb8b916ebac85d2f1691e6ead189c14
Thomas Pieters <pieters.tg@gmail.com>
2021-05-14 13:02:15 +0200

    Manager Unasign btn functionality

--- src/Controller/ManagerController.php
+++ src/Controller/ManagerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TicketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +15,26 @@ class ManagerController extends AbstractController
         return $this->render('manager/index.html.twig', [
             'controller_name' => 'ManagerController',
         ]);
+    }
+
+    #[Route('/un-assign-all/', name: 'unassign_all')]
+    public function unAssignAll(TicketRepository $repository): Response
+    {
+        $tickets = $repository->findAll();
+        $entityManager = $this->getDoctrine()->getManager();
+
+        foreach ($tickets as $ticket)
+        {
+            if($ticket->getAssignedAgent() !== null && in_array($ticket->getStatus(), [2, 3], true))
+            {
+                $ticket->setAssignedAgent(null);
+                $ticket->setStatus(1);
+                $entityManager->persist($ticket);
+            }
+        }
+        $entityManager->flush();
+
+        return $this->redirectToRoute('manager');
+
     }
 }
